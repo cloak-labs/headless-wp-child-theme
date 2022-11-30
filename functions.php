2d127a7272ed12999bfafef5b50ea69c90c74637
Wade Croft <wadecroft@gmail.com>
2022-11-30 14:26:02 -0800

    add code from newer projects functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ function get_frontend_url()
   return 'https://my-project.vercel.app';
 }
 
-
 add_action('enqueue_block_editor_assets', 'gutenberg_editor_assets');
 function gutenberg_editor_assets()
 {
@@ -32,6 +31,9 @@ function register_menus()
   );
 }
 
+add_filter('acf/settings/rest_api_format', function () {
+  return 'standard';
+});
 
 add_action('init', 'register_blocks');
 function register_blocks()
@@ -70,14 +72,57 @@ function preview_url()
   return "{$front_end_url}/api/preview?id={$id}&parent={$parent}&secret={$secret}";
 }
 
-// Incremental static regeneration on save
+// On-demand Incremental Static Regeneration (ISR) --> rebuild static pages/posts immediately upon saving your changes in WP
 add_action('save_post_page', 'revalidate_on_save', 10, 2);
 function revalidate_on_save($post_ID, $post)
 {
   $front_end_url = get_frontend_url();
+
+  // manually add environment URLs to this array if you wish to enable on-demand ISR for that environment (useful for testing one-off Vercel deployments or when running a production build locally) 
+  $environments_to_revalidate = [$front_end_url, "http://localhost:3000"];
+
   $slug = $post->post_name;
-  $response = wp_remote_get("{$front_end_url}/api/revalidate/{$slug}?secret=Z87ZfKnwgE9Jf3q6zaFjalw2");
+  $type = $post->post_type;
+
+  foreach ($environments_to_revalidate as $url) {
+    $response = wp_remote_get("{$url}/api/revalidate/{$slug}?post_type={$type}&secret=Z87ZfKnwgE9Jf3q6zaFjalw2");
+  }
 }
+
+// Brought in from lionheart
+add_filter('jwt_auth_iss', function () {
+  // Default value is get_bloginfo( 'url' );
+  return 'http://wordpress-admin-url';
+});
+
+// Brought in from lionheart
+add_action('rest_api_init', function () {
+  register_rest_route('wp/v2', 'menu', array(
+    'methods' => 'GET',
+    'callback' => 'custom_wp_menu',
+  ));
+});
+
+// create custom function to return nav menu
+function custom_wp_menu() {
+	// Replace your menu name, slug or ID carefully
+	return wp_get_nav_menu_items('Navbar');
+}
+
+// brought in from lionheart
+add_filter('register_post_type_args', 'wpd_change_post_type_args', 10, 2);
+function wpd_change_post_type_args($args, $post_type)
+{
+  if ('testimonials' == $post_type) {
+    $args['rewrite']['with_front'] = false;
+    $args['rewrite']['slug'] = 'testimonials';
+  } else if ('faqs' == $post_type) {
+    $args['rewrite']['with_front'] = false;
+    $args['rewrite']['slug'] = 'faqs';
+  }
+  return $args;
+}
+
 
 add_filter('jwt_auth_expire', 'on_jwt_expire_token', 10, 1);
 function on_jwt_expire_token($exp)
@@ -88,6 +133,7 @@ function on_jwt_expire_token($exp)
   return $exp;
 }
 
+// Helper function to write data to debug.log file
 if (!function_exists('write_log')) {
   function write_log($log)
   {
